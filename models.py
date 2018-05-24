6bb44862d816211b13e1a7e30f9ef23e88626598
amznero <398994055@qq.com>
2018-05-25 01:36:04 +0800

    update

---
+++ models.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet(nn.Module):
+    def __init__(self, ):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16*4*4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.classify = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.avg_pool2d(out, kernel_size=2, stride=2)
+        out = F.relu(self.conv2(out))
+        out = F.avg_pool2d(out, kernel_size=2, stride=2)
+        out = out.view(-1, 16*4*4)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+
+        return self.classify(out)
+
+if __name__ == '__main__':
+    from torch.autograd import Variable
+    model = LeNet()
+    ipt = Variable(torch.randn(16, 1, 28, 28))
+    out = model(ipt)
+    print(out.size())
