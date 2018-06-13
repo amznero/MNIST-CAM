056a4e3acc74ae21323e6a8fcf598d3e4e2c0f44
amznero <398994055@qq.com>
2018-06-14 00:25:20 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import cv2
 import torch
 import argparse
+import numpy as np
 import torch.nn as nn
-from models import LeNet, Net
+from PIL import Image
+from models import LeNet
 import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 def train(args, model, train_loader, optimizer, epoch):
     model.train()
+    
     for batch_idx, (data, target) in enumerate(train_loader):
         target = target.cuda(async=True)
         data = torch.autograd.Variable(data)
@@ -24,13 +28,15 @@ def train(args, model, train_loader, optimizer, epoch):
 
 def test(args, model, test_loader):
     model.eval()
+
     test_loss = 0
     correct = 0
+    
     with torch.no_grad():
         for data, target in test_loader:
             target = target.cuda(async=True)
-            data = torch.autograd.Variable(data, volatile=True)
-            target = torch.autograd.Variable(target, volatile=True)
+            # data = torch.autograd.Variable(data, volatile=True)
+            # target = torch.autograd.Variable(target, volatile=True)
 
             output = model(data)
             test_loss += F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
@@ -41,6 +47,32 @@ def test(args, model, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+def cam(model, epoch):
+    model.eval()
+    with torch.no_grad():
+        img_0 = Image.open('imgs/0.jpg')
+        transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                                    ])
+        tensor_0 = transform(img_0)
+        tensor_0 = tensor_0.view(1, 1, 28, 28)
+        output_0 = model(tensor_0)
+        para = list(model.parameters())[-2][0:1]
+        para = para.data.cpu().numpy()
+        global feature_blob
+        cam_feat = feature_blob[0].view(16, -1).data.cpu().numpy()
+        cam = np.matmul(para, cam_feat)[0].reshape(8, 8)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        cam_img = np.uint8(255 * cam_img)
+        output_cam = cv2.resize(cam_img, (28, 28))
+        heatmap = cv2.applyColorMap(output_cam, cv2.COLORMAP_JET)
+        img = cv2.imread('imgs/0.jpg')
+        save_img = heatmap*0.3 + img*0.5
+        save_img = cv2.resize(save_img, (224, 224))
+        cv2.imwrite('result/cam_{}.jpg'.format(epoch), save_img)
 
 
 def main():
@@ -82,16 +114,22 @@ def main():
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    feature_blob = np.zeros([1, 16, 8, 8])
 
     model = LeNet()
+    def hook(module, input, output):
+        global feature_blob
+        feature_blob = output
+
+    model._modules.get('conv2').register_forward_hook(hook)
     model = torch.nn.DataParallel(model).cuda()
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, train_loader, optimizer, epoch)
+        cam(model, epoch)
         test(args, model, test_loader)
 
 
 if __name__ == '__main__':
     main()
-
--- models.py
+++ models.py
@@ -7,19 +7,19 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.fc1 = nn.Linear(16*4*4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.classify = nn.Linear(84, 10)
+        # self.fc1 = nn.Linear(16, 10)
+        # self.fc2 = nn.Linear(120, 84)
+        self.classify = nn.Linear(16, 10)
 
     def forward(self, x):
         out = F.relu(self.conv1(x))
         out = F.max_pool2d(out, kernel_size=2)
         out = F.relu(self.conv2(out))
-        out = F.max_pool2d(out, kernel_size=2)
-        print(out.size())
+        out = F.relu(F.avg_pool2d(out, kernel_size=8))
+        # out = F.avg_pool2d(out, kernel_size=4)
         out = out.view(out.size(0), -1)
-        out = F.relu(self.fc1(out))
-        out = F.relu(self.fc2(out))
+        # out = F.relu(self.fc1(out))
+        # out = F.relu(self.fc2(out))
 
         return self.classify(out)
 
